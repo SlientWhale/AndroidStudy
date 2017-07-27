a881d76e59799a798b4340d58822d3370efc3717
slientwhale <18033405861@163.com>
2017-07-27 20:55:21 +0800

    去掉不必要的文件

--- view/example/app/src/main/java/com/slientwhale/www/example/DemoListActivity.java
+++ view/example/app/src/main/java/com/slientwhale/www/example/DemoListActivity.java
@@ -1,7 +1,7 @@
 package com.slientwhale.www.example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class DemoListActivity extends AppCompatActivity {
 

bfd84ffb65320e15c2b655031a20c94ee1adcde0
slientwhale <18033405861@163.com>
2017-07-31 18:40:06 +0800

    新增说明文件

--- view/example/app/src/main/java/com/slientwhale/www/example/DemoListActivity.java
+++ view/example/app/src/main/java/com/slientwhale/www/example/DemoListActivity.java
@@ -1,13 +1,54 @@
 package com.slientwhale.www.example;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class DemoListActivity extends AppCompatActivity {
+import com.slientwhale.www.example.base.BaseActivity;
+import com.slientwhale.www.example.ui.ViewDemo01_paint_Activity;
+import com.slientwhale.www.example.ui.adapter.DemoListAdapter;
+import com.slientwhale.www.example.widget.BaseRecycleView;
+import com.slientwhale.www.example.widget.listener.BaseRecycleListener;
+
+import java.util.Arrays;
+
+public class DemoListActivity extends BaseActivity implements BaseRecycleListener{
+
+    private BaseRecycleView<String> recycleView;
+    private DemoListAdapter<String> adapter;
+    private String[] item;
+    private Class[] classs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_demo_list);
+        adapter = new DemoListAdapter<>(this.getBaseContext());
+        recycleView.setAdapter(adapter);
+        initData();
+    }
+
+    //创建项目
+    private void initData() {
+        item = new String[]{"Paint 的用法"};
+        classs = new Class[]{ViewDemo01_paint_Activity.class};
+        recycleView.addItems(Arrays.asList(item));
+    }
+
+    @Override
+    public View addContainer() {
+        recycleView = new BaseRecycleView(this.getBaseContext());
+        return recycleView;
+    }
+
+
+    @Override
+    public void onItemClick(int position, Object data) {
+        Intent intent = new Intent(this.getBaseContext(),classs[position]);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onItemLongClick(int position, Object data) {
+
     }
 }
